5c6c57ab6076bae84076bc6da37e554182ea2817
#f0f3e3 <170834894+osiristape@users.noreply.github.com>
2024-10-26 20:19:27 +0800

    config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+ADC_ENDPOINTS = [
+    'https://example-adc1.com', #modified this
+    'https://example-adc2.com'
+]
+
+ALERT_THRESHOLD = {
+    'latency': 300  
+}
+
+ALERT_EMAIL = "alert@example.com"
+SMTP_SERVER = "smtp.example.com"
+SMTP_PORT = 587
+SMTP_USER = "user@example.com"
+SMTP_PASSWORD = "password"
